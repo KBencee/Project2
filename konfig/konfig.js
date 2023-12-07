f13af9312153df47cbb9be5664d868a809761995
K.Bence <90392576+KBencee@users.noreply.github.com>
2023-12-07 11:37:31 +0100

    Add files via upload

---
+++ konfig/konfig.js
@@ -0,0 +1,34 @@
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function onlyOne2(checkbox) {
+    var checkboxes = document.getElementsByName('check2')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function onlyOne3(checkbox) {
+    var checkboxes = document.getElementsByName('check3')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function onlyOne4(checkbox) {
+    var checkboxes = document.getElementsByName('check4')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function onlyOne5(checkbox) {
+    var checkboxes = document.getElementsByName('check5')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
