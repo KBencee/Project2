a774c22b87e92002ba2986e27b38786ffea2f3af
K.Bence <90392576+KBencee@users.noreply.github.com>
2023-12-14 08:56:56 +0100

    Add files via upload

--- konfig/konfig.js
+++ konfig/konfig.js
@@ -32,3 +32,32 @@ function onlyOne5(checkbox) {
         if (item !== checkbox) item.checked = false
     })
 }
+
+let list1=["","","","","",""]
+
+belerak(0)
+function belerak(i){
+    index = Number(i)
+    kiir()
+}
+function kiir(){
+    
+ switch(index)
+ {
+    case 0:
+        document.getElementById("cuccok").innerHTML="<p>A Lol egy 5v5 moba játék</p> <br>" + `<p>${list[index]} </p>`
+        break;
+    case 1:
+        document.getElementById("cuccok").innerHTML="<p>5v5 Tactial shooter</p> <br>" + `<p>${list[index]} </p>`
+        break;
+    case 2:
+        document.getElementById("cuccok").innerHTML="<p>Cartony style, looter shooter</p> <br>" + `<p>${list[index]} </p>`
+        break;
+    case 3:
+        document.getElementById("cuccok").innerHTML="<p>Fast paced storygame</p> <br>" + `<p>${list[index]} </p>`
+        break;
+    case 4:
+        document.getElementById("cuccok").innerHTML="<p>kockák</p> <br>" + `<p>${list[index]} </p>`
+        break;
+ }
+}
